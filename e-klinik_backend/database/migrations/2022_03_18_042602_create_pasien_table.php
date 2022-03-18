6962ea0706a46c86b980ab30929ae5bf2d2cffed
SriCahyani16 <srcahyani1616@gmail.com>
2022-03-18 12:55:17 +0700

    create: dokter,pasien,jadwal_dokter migration

---
+++ e-klinik_backend/database/migrations/2022_03_18_042602_create_pasien_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+
+            $table->string('nik')->unique();
+            $table->foreignId('id_user');
+            $table->string('nama_depan');
+            $table->string('nama_belakang');
+            $table->string('alamat_rumah');
+            $table->string('usia');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->datetime('tgl_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_pasien');
+    }
+};
